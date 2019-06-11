d79a98f3280c19b10e2de55b29f26ec5c83c53e9
fdc-amabele <fdc.refugio@gmail.com>
2019-06-11 16:49:38 +0800

    update

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         //test
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+    }
 
 
 }
